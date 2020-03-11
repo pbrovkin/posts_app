8d1982a9346b40b92bf2f0d65e8e80050621d9fe
AtikRahman <atik321@gmail.com>
2020-03-11 12:15:47 +0200

    add styling

--- src/App.js
+++ src/App.js
@@ -1,62 +1,75 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import PostList from './components/PostList'
-import Post from './components/Post'
-import AddPostForm from './components/AddPostForm'
-import Menu from './components/Menu'
-import Notification from './components/Notification'
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PostList from "./components/PostList";
+import Post from "./components/Post";
+import AddPostForm from "./components/AddPostForm";
+import Menu from "./components/Menu";
+import Notification from "./components/Notification";
+import Header from "./components/Header";
 
 const App = () => {
-    const [posts, setPosts] = useState([
-        {
-            title: 'My day in integrify',
-            category: 'work',
-            content: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
-            id: '1',
-        },
-        {
-            title: 'My talk at React Meetup',
-            category: 'speech',
-            content: 'Something content here. Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
-            id: '2',
-        },
-        {
-            title: 'Running in the forest',
-            category: 'sport',
-            content: 'Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content',
-            id: '3',
-        }
-    ])
-
-    const [notification, setNotification] = useState('')
-
-    const addPost = (post) => {
-        post.id = (Math.random() * 100000).toFixed(0)
-        setPosts(posts.concat(post))
-        setNotification(`'${post.title}' added!`)
-        setTimeout(() => {
-            setNotification("")
-        }, 10000)
+  const [posts, setPosts] = useState([
+    {
+      title: "My day in integrify",
+      category: "work",
+      content:
+        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
+      id: "1"
+    },
+    {
+      title: "My talk at React Meetup",
+      category: "speech",
+      content:
+        "Something content here. Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content",
+      id: "2"
+    },
+    {
+      title: "Running in the forest",
+      category: "sport",
+      content:
+        "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content",
+      id: "3"
     }
+  ]);
 
-    const postById = (id) => posts.find(post => post.id === id)
+  const [notification, setNotification] = useState("");
 
-    return (
-        <>
-            <Router>
-                <div>
-                    <Menu />
-                    <Notification message={notification} />
-                    <Switch>
-                        <Route path="/" exact render={() => <PostList posts={posts} />} />
-                        <Route exact path="/posts/:id" render={({ match }) => <Post post={postById(match.params.id)} />} />
-                        <Route path="/addPost" render={(props) => <AddPostForm history={props.history} addPost={addPost} />} />
-                    </Switch>
-                </div>
-            </Router>
-        </>
-    )
-}
+  const addPost = post => {
+    post.id = (Math.random() * 100000).toFixed(0);
+    setPosts(posts.concat(post));
+    setNotification(`'${post.title}' added!`);
+    setTimeout(() => {
+      setNotification("");
+    }, 10000);
+  };
 
-export default App
+  const postById = id => posts.find(post => post.id === id);
+
+  return (
+    <>
+      <Header />
+      <Router>
+        <div>
+          <Menu />
+          <Notification message={notification} />
+          <Switch>
+            <Route path="/" exact render={() => <PostList posts={posts} />} />
+            <Route
+              exact
+              path="/posts/:id"
+              render={({ match }) => <Post post={postById(match.params.id)} />}
+            />
+            <Route
+              path="/addPost"
+              render={props => (
+                <AddPostForm history={props.history} addPost={addPost} />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+};
+
+export default App;
--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,41 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const AddPostForm = (props) => {
-    const [title, setTitle] = useState('')
-    const [category, setCategory] = useState('')
-    const [content, setContent] = useState('')
+const AddPostForm = props => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [content, setContent] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        props.addPost({
-            title,
-            category,
-            content,
-        })
-        props.history.push('/');
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.addPost({
+      title,
+      category,
+      content
+    });
+    props.history.push("/");
+  };
 
-    return (
+  return (
+    <div>
+      <h3>add a post</h3>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h3>add a post</h3>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    title
-            <input name='title' value={title} onChange={(e) => setTitle(e.target.value)} />
-                </div>
-                <div>
-                    category
-            <input name='category' value={category} onChange={(e) => setCategory(e.target.value)} />
-                </div>
-                <div>
-                    content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-                </div>
-                <button>add</button>
-            </form>
+          title
+          <input
+            name="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
         </div>
-    )
+        <div>
+          category
+          <input
+            name="category"
+            value={category}
+            onChange={e => setCategory(e.target.value)}
+          />
+        </div>
+        <div>
+          content
+          <input
+            name="content"
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
+        </div>
+        <button>add</button>
+      </form>
+    </div>
+  );
+};
 
-}
-
-export default AddPostForm
+export default AddPostForm;
