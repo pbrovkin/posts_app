2f8ecefdb5e3ee2522276744261fba45335bf285
pbrovkin <pbrovkin@gmail.com>
2020-03-12 11:00:27 +0200

    notif color changes

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const Notification = ({ message }) => <div className="container">
-    <p className ="notification">{message}</p>
-</div>
+const Notification = ({ message }) => {
+    const notifStyle = {
+        color: ''
+    }
+    if (message.includes('added')) {
+        notifStyle.color = 'green'
+    }
+    if (message.includes('deleted')) {
+        notifStyle.color = 'red'
+    }
+    return (
+        <div style={notifStyle} className="container">
+            <p className="notification">{message}</p>
+        </div>
+    )
+}
 
 export default Notification
